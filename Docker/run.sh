844a842731b7f59205508f5d896a294b2695bc83
Shumpe Morita <65478897+shumpe-m@users.noreply.github.com>
2023-05-29 16:43:36 +0900

    fix run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,5 +1,5 @@
 docker run --gpus all --rm -it --ipc host \
-    --network mongodb-network \
+    --net host \
     --volume="/dev:/dev" \
     --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY \
